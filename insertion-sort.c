1417da7bcb4224a8dd2c83c3c1e85e963444e11a
adityadaharwal963 <121679194+adityadaharwal963@users.noreply.github.com>
2023-04-09 14:07:51 +0530

    Update insertion-sort.c

--- insertion-sort.c
+++ insertion-sort.c
@@ -3,11 +3,11 @@
 void insertionsort(int a[],int n){
     int t;
   for(int i=0;i<n-1;i++){
-    int key=a[i];
+    int key=a[i];            // set key to compare
    int  j=i-1;
     while(j>=0 && key<a[j]){
         t=a[j];
-        a[j]=a[j+1];        /// swap elements
+        a[j]=a[j+1];           // swap elements
         a[j+1]=t;
         j--;                   // compare pervious elements
     }
@@ -16,13 +16,16 @@ void insertionsort(int a[],int n){
 int main(){
   int a[100],i=0;
   printf("enter element in array and exit by -1:\n");
+    //  read array from user
     do{
         scanf("%d",(a+i));
-        i++;  //count array element
+        i++;                       //count array element
     }
-    while(*(a+i-1)!=-1);// take input till arr element != -1
-  insertionsort(a,i);      
-  for(int j=0;j<i-1;j++){          // -1 not included
+    while(*(a+i-1)!=-1);           // take input till arr element != -1
+    
+  insertionsort(a,i);              // call function
+    
+  for(int j=0;j<i-1;j++){          // -1 not included  so  i-1 
     printf("%d ", a[j]);
   }
   return 0;
